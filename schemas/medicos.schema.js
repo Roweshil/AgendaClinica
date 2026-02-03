ebcfa48efdcced01a311ea3f5f42cb1abd38810e
Roweshil <x-vdm@hotmail.com>
2026-02-03 15:11:54 -0600

    ajuste de rutas para autenticación

--- schemas/medicos.schema.js
+++ schemas/medicos.schema.js
@@ -1,7 +1,11 @@
 import z   from 'zod'
+import { id } from 'zod/locales'
 
 const medicoSchema = z.object({
+    id: z.number().positive(),
     nombre: z.string().min(2).max(100),
-
-
+    cedula: z.string().min(5).max(20),
+    especialidad: z.string().min(2).max(100),
+    telefono: z.string().min(7).max(15),
+    email: z.string().email().max(100),
 })
